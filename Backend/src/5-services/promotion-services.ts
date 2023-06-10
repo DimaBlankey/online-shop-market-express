e273709241955522cf214081201033ac6f4b7d47
DimaBlankey <115228381+DimaBlankey@users.noreply.github.com>
2023-06-10 18:14:16 +0300

    promotions

--- Backend/src/5-services/promotion-services.ts
+++ Backend/src/5-services/promotion-services.ts
@@ -2,6 +2,7 @@ import { OkPacket } from "mysql";
 import appConfig from "../4-utils/app-config";
 import dal from "../4-utils/dal";
 import PromotionModel from "../2-models/promotion-model";
+import { ResourceNotFoundError } from "../2-models/client-errors";
 
 async function getAllPromotions(): Promise<PromotionModel[]> {
   const sql = `
@@ -12,6 +13,71 @@ async function getAllPromotions(): Promise<PromotionModel[]> {
   return promotions;
 }
 
+async function addPromotion(
+  promotion: PromotionModel
+): Promise<PromotionModel> {
+  // DO validations
+  const sql = `
+  INSERT INTO promotions VALUES(DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const isActive = false;
+  const result: OkPacket = await dal.execute(sql, [
+    promotion.name,
+    promotion.startDate,
+    promotion.endDate,
+    promotion.percentageDiscount,
+    promotion.amountDiscount,
+    promotion.finalPriceDiscount,
+    promotion.products,
+    isActive,
+  ]);
+
+  promotion.id = result.insertId;
+
+  return promotion;
+}
+
+async function promotionStatus(
+  promotion: PromotionModel
+): Promise<PromotionModel> {
+  // Do validation
+  const sql = `
+  UPDATE promotions SET
+  isActive = ?
+  WHERE id = ?
+  `;
+  const result: OkPacket = await dal.execute(sql, [
+    promotion.isActive,
+    promotion.id,
+  ]);
+
+  if (result.affectedRows === 0) {
+    throw new ResourceNotFoundError(promotion.id);
+  }
+
+  return promotion;
+}
+
+async function deletePromotion(id: number): Promise<void> {
+
+  const sqlCheck = `
+  SELECT isActive FROM promotions WHERE id = ?
+  `;
+  const status = await dal.execute(sqlCheck, [id]);
+
+  if (status[0].isActive === 1) {
+    return;
+  }
+
+  const sql = `
+  DELETE FROM promotions WHERE id = ?
+  `;
+  const result: OkPacket = await dal.execute(sql, [id]);
+  if (result.affectedRows === 0) throw new ResourceNotFoundError(id);
+}
+
 export default {
   getAllPromotions,
+  addPromotion,
+  promotionStatus,
+  deletePromotion,
 };
--- Backend/src/6-routes/promotion-routes.ts
+++ Backend/src/6-routes/promotion-routes.ts
@@ -1,21 +1,67 @@
 import express, { Request, Response, NextFunction } from "express";
 import verifyAdmin from "../3-middleware/verify-admin";
 import promotionServices from "../5-services/promotion-services";
-
+import PromotionModel from "../2-models/promotion-model";
 
 const router = express.Router();
 
 router.get(
-    "/promotions/",
-    verifyAdmin,
-    async (request: Request, response: Response, next: NextFunction) => {
-      try {
-        const promotions = await promotionServices.getAllPromotions();
-        response.json(promotions);
-      } catch (err: any) {
-        next(err);
-      }
+  "/promotions/",
+  verifyAdmin,
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const promotions = await promotionServices.getAllPromotions();
+      response.json(promotions);
+    } catch (err: any) {
+      next(err);
     }
-  );
+  }
+);
+
+router.post(
+  "/promotions",
+  verifyAdmin,
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const promotion = new PromotionModel(request.body);
+      const addedPromotion = await promotionServices.addPromotion(promotion);
+      response.status(201).json(addedPromotion);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/promotions/:id",
+  verifyAdmin,
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      request.body.id = +request.params.id;
+      const promotion = new PromotionModel(request.body);
+      const updatedPromotion = await promotionServices.promotionStatus(promotion);
+      response.json(updatedPromotion);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+);
+
+router.delete(
+  "/promotions/:id",
+  verifyAdmin,
+  async (request: Request, response: Response, next: NextFunction) => {
+    try {
+      const id = +request.params.id;
+      await promotionServices.deletePromotion(id)
+      response.sendStatus(204);
+    } catch (err: any) {
+      next(err);
+    }
+  }
+);
+
+
+
 
 export default router;

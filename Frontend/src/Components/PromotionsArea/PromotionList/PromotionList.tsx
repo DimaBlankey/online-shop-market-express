93ba64662c29591b1f9d7c3ec45d7af3c9b09f02
DimaBlankey <115228381+DimaBlankey@users.noreply.github.com>
2023-06-16 20:06:09 +0300

    promotion list

--- Frontend/src/Components/PromotionsArea/PromotionList/PromotionList.tsx
+++ Frontend/src/Components/PromotionsArea/PromotionList/PromotionList.tsx
@@ -3,63 +3,95 @@ import "./PromotionList.css";
 import PromotionModel from "../../../Models/PromotionModel";
 import promotionService from "../../../Services/PromotionService";
 import notifyService from "../../../Services/NotifyService";
-import { Card, CardContent, List, ListItem, Typography } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import { Switch } from "@mui/material";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
 
 function PromotionList(): JSX.Element {
+  const [promotions, setPromotions] = useState<PromotionModel[]>([]);
 
-    const [promotions, setPromotions] = useState<PromotionModel[]>([])
+  useEffect(() => {
+    promotionService
+      .getAllPromotions()
+      .then((responsePromotions) => {
+        setPromotions(responsePromotions);
+      })
+      .catch((err) => notifyService.error(err));
+  }, []);
 
-    useEffect(() => {
-          promotionService
-            .getAllPromotions()
-            .then((responsePromotions) => {
-                setPromotions(responsePromotions);
-            })
-            .catch((err) => notifyService.error(err));
-      }, []);
-
-    return (
-        <div className="PromotionList scrollbar">
-			<Typography variant="h4">Promotions</Typography>
+  return (
+    <div className="PromotionList scrollbar">
+      <Typography variant="h4">Promotions</Typography>
       {promotions.length > 0 ? (
         promotions.map((promotion, index) => {
           const products = JSON.parse(promotion.products);
-            console.log(products)
+          console.log(products);
           return (
             <Card key={index} sx={{ margin: "10px" }}>
+              <FormControl component="fieldset">
+                <Grid container alignItems="center" spacing={10}>
+                  <Grid item>
+                    <FormGroup aria-label="position" row>
+                      <FormControlLabel
+                        value="active"
+                        control={<Switch color="primary" />}
+                        label="Activate"
+                        labelPlacement="start"
+                      />
+                    </FormGroup>
+                  </Grid>
+                  <Grid item>
+                    <Button variant="text" color="error">
+                      DELETE
+                    </Button>
+                  </Grid>
+                </Grid>
+              </FormControl>
+
               <CardContent>
                 <Typography variant="h5" component="div">
                   {promotion.name}
                 </Typography>
-                <Typography variant="body2" component="div">
-                  Start Date: {new Date(promotion.startDate).toLocaleDateString()}
+                <Typography variant="inherit" component="div">
+                  Start Date:{" "}
+                  {new Date(promotion.startDate).toLocaleDateString()}
                 </Typography>
-                <Typography variant="body2">
+                <Typography variant="inherit">
                   End Date: {new Date(promotion.endDate).toLocaleDateString()}
                 </Typography>
                 <Typography variant="h6">Discount Type</Typography>
-                {promotion?.percentageDiscount > 0 &&(
-                <Typography variant="body2">
-                 -{promotion.percentageDiscount}% off
-                </Typography>
+                {promotion?.percentageDiscount > 0 && (
+                  <Typography variant="subtitle1">
+                    -{promotion.percentageDiscount}% off
+                  </Typography>
                 )}
-                {promotion?.amountDiscount > 0 &&(
-                <Typography variant="body2">
-                  -{promotion.amountDiscount}$ off
-                </Typography>
+                {promotion?.amountDiscount > 0 && (
+                  <Typography variant="subtitle1">
+                    -{promotion.amountDiscount}$ off
+                  </Typography>
                 )}
-                {promotion?.finalPriceDiscount > 0 &&(
-                <Typography variant="body2">
-                  Products for {promotion.finalPriceDiscount}$
-                </Typography>
+                {promotion?.finalPriceDiscount > 0 && (
+                  <Typography variant="subtitle1">
+                    Products for {promotion.finalPriceDiscount}$
+                  </Typography>
                 )}
-                <Typography  variant="h6" component="div">Products</Typography>
+                <Typography variant="h6" component="div">
+                  Products
+                </Typography>
                 <List>
                   {products.map((product: any, productIndex: number) => (
                     <ListItem key={productIndex}>
-                      <Typography variant="subtitle1">
-                        {product}
-                      </Typography>
+                      <Typography variant="subtitle1">{product}</Typography>
                     </ListItem>
                   ))}
                 </List>
@@ -70,8 +102,8 @@ function PromotionList(): JSX.Element {
       ) : (
         <div>No promotions yet...</div>
       )}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default PromotionList;
